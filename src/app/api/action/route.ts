aa2e87a8ce1b4176c45f545c8c490a2a6162de50
Sarthak2 <aniki123.ai@gmail.com>
2024-10-02 16:38:22 +0530

    fix

--- src/app/api/action/route.ts
+++ src/app/api/action/route.ts
@@ -4,16 +4,16 @@ import { ActionGetResponse, ACTIONS_CORS_HEADERS } from "@solana/actions";
 export async function GET(req: NextRequest) {
   let response = {
     type: "action",
-    icon: `https://pbs.twimg.com/media/GVtOC-KXQAA1aN7?format=jpg&name=large`,
+    icon: `https://preview.redd.it/plinko-v0-w04f863eh2lc1.jpeg?width=1080&crop=smart&auto=webp&s=fac57c67ec4ba493ddddad815e340753a0e5f625`,
     media: `https://video.twimg.com/amplify_video/1815225283353673728/vid/avc1/1280x720/33jn7JEVyOGfRh21.mp4`,
     mediaType: "video",
-    title: "Action A",
-    description: "Action A",
-    label: "Action A Label",
+    title: "Play Plinko",
+    description: "Play plinko using blinks",
+    label: "Bet 0.5 SOL",
     links: {
       actions: [
         {
-          label: "Blinks are blinking", // button text
+          label: "Bet 0.5 SOL", // button text
           href: "/api/action/", // api endpoint
         },
       ],
